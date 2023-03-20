e60e89a6950d209338274e6594c014f8033032ec
Nick Jarvis <najarvis2016@gmail.com>
2023-03-20 00:19:03 +0000

    Improves key searching, no longer crashes on the mammal madness powerpoint

--- index.js
+++ index.js
@@ -18,18 +18,45 @@ function send_index(index_html, res) {
     res.end();
 }
 
+function find_obj_with_key(obj, key, func) {
+    // Iterate deeply through an object, searching for a specified key
+    // if a function is provided, it will run on every object that
+    // key is found in. Otherwise it will return the first sub object containing that key
+    // TODO: This probably won't find the same key if it was nested inside one, this is not a problem for my use case
+    stack = [obj];
+
+    do {
+        cur_obj = stack.pop()
+        for (cur_key in cur_obj) {
+            if (cur_key == key) {
+                if (func == undefined) {
+                    return cur_obj;
+                }
+                else {
+                    func(cur_obj)
+                }
+            } else if (typeof cur_obj[cur_key] === "object") {
+                stack.push(cur_obj[cur_key]);
+            }
+        }
+    } while (stack.length > 0);
+    return null;
+}
+
 async function translate_file(filepath) {
     text_id = 0
     // const filepath = files.multipleFiles.filepath;
-    console.log("Converting pptx to JSON")
+    console.time("Converting pptx to JSON")
     working_json = await pptx2json.toJson(filepath);
+    console.timeEnd("Converting pptx to JSON")
     text_to_translate = []
     extracted_spaces = []
     // A bit wasteful, but we can leverage the recursive function within JSON.stringify to find the text within each page, as well as putting it back.
     // First, grab all of the text within the "a:t" tags
-    console.log("First stringification...")
-    JSON.stringify(working_json, (key, nested_obj) => {
-        if (nested_obj && nested_obj["a:t"] != undefined) {
+    console.time("First stringification")
+
+    find_obj_with_key(working_json, "a:t", (nested_obj) => {
+        if (nested_obj["a:t"] != undefined) {
             txt = nested_obj["a:t"][0];
             if (txt.length > 0) {
                 nested_obj.text_id = text_id;
@@ -44,26 +71,27 @@ async function translate_file(filepath) {
                 extracted_spaces.push([pre_spaces, post_spaces]);
             }
         }
-        return nested_obj;
     });
+    console.timeEnd("First stringification")
 
     // joined_text = text_to_translate.join(sep);
-    console.log({"Extracted text": text_to_translate});
+    //console.log({"Extracted text": text_to_translate});
 
     // Then, translate all the text in one bulk translate
-    console.log("Sending to Google for translations...")
+    console.time("Sending to Google for translations")
     const res = await translate(text_to_translate, { from: 'en', to: 'es' });
+    console.timeEnd("Sending to Google for translations")
     translated_text = []
     res.forEach((val, idx) => {
         translated_text.push(val.text)
     })
 
     text_id = undefined;
-
-    console.log("Second stringification...");
+    
+    console.time("Second stringification");
     // Finally, insert the translated text back into 
-    JSON.stringify(working_json, (key, nested_obj) => {
-        if (nested_obj && nested_obj["a:t"] != undefined && nested_obj.text_id != undefined) {
+    find_obj_with_key(working_json, "a:t", (nested_obj) => {
+        if (nested_obj["a:t"] != undefined && nested_obj.text_id != undefined) {
             if (text_id == undefined) {
                 text_id = nested_obj.text_id;
             }
@@ -73,12 +101,13 @@ async function translate_file(filepath) {
             text_to_insert = pre_spaces + translated_string + post_spaces;
             nested_obj["a:t"] = [text_to_insert];
         }
-        return nested_obj;
     });
+    console.timeEnd("Second stringification");
 
-    console.log("Converting back into a pptx file, overwriting the previous file");
+    console.time("Converting back into a pptx file, overwriting the previous file");
     // Currently overwrite the uploaded file. I'm sure this won't have any negative consequences /s
     await pptx2json.toPPTX(working_json, {'file': filepath});
+    console.timeEnd("Converting back into a pptx file, overwriting the previous file");
 }
 
 function accept_form_data(req, res) {
@@ -90,8 +119,9 @@ function accept_form_data(req, res) {
         // Parse and save any form data
         const form = formidable({ multiples: true });
 
-        console.log("File received, attempting to parse...")
+        console.time("File received, attempting to parse")
         form.parse(req, (err, fields, files) => {
+            console.timeEnd("File received, attempting to parse")
             if (err) {
                 res.writeHead(err.httpCode || 400, {'Content-Type': 'text/plain' });
                 res.end(String(err));
@@ -99,11 +129,12 @@ function accept_form_data(req, res) {
             }
             if (files.multipleFiles.length == undefined) {
                 // Dealing with a single file
-                console.log("Working with a single file, beginning file translation process...")
+                console.time("Beginning file translation process")
                 const filepath = files.multipleFiles.filepath;
                 
                 translate_file(filepath).then(() => {
-                    console.log("Translation complete, sending the data back to the user!");
+                    console.timeEnd("Beginning file translation process")
+                    console.time("Translation complete, sending the data back to the user!");
                     stat = filesystem.statSync(filepath);
                     res.writeHead(200, { 
                         'Content-Length': stat.size, 
@@ -112,6 +143,7 @@ function accept_form_data(req, res) {
                     });
                     readStream = filesystem.createReadStream(filepath)
                     readStream.pipe(res);
+                    console.timeEnd("Translation complete, sending the data back to the user!");
                 });
             }
             else {
